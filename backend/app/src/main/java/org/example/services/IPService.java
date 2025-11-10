ca3a2cfa3275ee2ad1bbdd0a356c0f0418f40dc7
JeffTunner <abhinavkaushik05@gmail.com>
2025-11-10 18:54:19 +0530

    Added error handling in the ip service

--- backend/app/src/main/java/org/example/services/IPService.java
+++ backend/app/src/main/java/org/example/services/IPService.java
@@ -15,8 +15,21 @@ public class IPService {
     private final RestTemplate restTemplate = new RestTemplate();
 
     public Map<String, Object> getIPInfo(String ip) {
-        String url = "https://geo.ipify.org/api/v2/country,city?apiKey=" + apiKey + "&ipAddress=" + ip;
-        ResponseEntity<Map> response = restTemplate.getForEntity(url, Map.class);
-        return response.getBody();
+        try {
+            String url = "https://geo.ipify.org/api/v2/country,city?apiKey=" + apiKey + "&ipAddress=" + ip;
+            ResponseEntity<Map> response = restTemplate.getForEntity(url, Map.class);
+            if(response.getStatusCode().is2xxSuccessful() && response.getBody() != null) {
+                return response.getBody();
+            } else {
+                throw new RuntimeException("Failed to fetch data: " +response.getStatusCode());
+            }
+        } catch (Exception e) {
+            return Map.of(
+                    "error", true,
+                    "message", "Could not fetch IP Info :" +e.getMessage()
+            );
+        }
+
+
     }
 }
