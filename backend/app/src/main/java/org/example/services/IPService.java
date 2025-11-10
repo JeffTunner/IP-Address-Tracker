9968eafb983b22c5e8ed5dbd3370f1188da9528c
JeffTunner <abhinavkaushik05@gmail.com>
2025-11-11 00:31:18 +0530

    search history saved in table format

--- backend/app/src/main/java/org/example/services/IPService.java
+++ backend/app/src/main/java/org/example/services/IPService.java
@@ -1,5 +1,8 @@
 package org.example.services;
 
+import org.example.models.SearchHistory;
+import org.example.repositories.SearchHistoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -14,10 +17,16 @@ public class IPService {
 
     private final RestTemplate restTemplate = new RestTemplate();
 
+    @Autowired
+    private SearchHistoryRepository historyRepository;
+
     public Map<String, Object> getIPInfo(String ip) {
         try {
             String url = "https://geo.ipify.org/api/v2/country,city?apiKey=" + apiKey + "&domain=" + ip;
             ResponseEntity<Map> response = restTemplate.getForEntity(url, Map.class);
+
+            historyRepository.save(new SearchHistory(ip));
+
             if(response.getStatusCode().is2xxSuccessful() && response.getBody() != null) {
                 return response.getBody();
             } else {
