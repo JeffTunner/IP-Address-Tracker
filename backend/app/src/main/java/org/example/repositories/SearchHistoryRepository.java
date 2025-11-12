aca1132c9f911c3148f6dc0150dda13567473531
JeffTunner <abhinavkaushik05@gmail.com>
2025-11-12 19:27:19 +0530

    Added a fucntion to return top 5 recent searches

--- backend/app/src/main/java/org/example/repositories/SearchHistoryRepository.java
+++ backend/app/src/main/java/org/example/repositories/SearchHistoryRepository.java
@@ -3,5 +3,9 @@ package org.example.repositories;
 import org.example.models.SearchHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SearchHistoryRepository extends JpaRepository<SearchHistory, Long> {
+
+    List<SearchHistory> findTop5ByOrderBySearchedAtDesc();
 }
