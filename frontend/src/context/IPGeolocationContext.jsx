d88ba3ba51600dc46a77fa13b7cd651b3bf72e63
JeffTunner <abhinavkaushik05@gmail.com>
2025-11-12 20:14:03 +0530

    Integrated recent searches with frontend

--- frontend/src/context/IPGeolocationContext.jsx
+++ frontend/src/context/IPGeolocationContext.jsx
@@ -28,16 +28,32 @@ export function IPGeolocationProvider({children}) {
     const apiKey = import.meta.env.VITE_API_KEY;
 
     async function fetchGeolocationData(IPAddress) {
-        let url = `http://localhost:8080/api/ip?ip=${IPAddress}`;
-        const response = await fetch(url);
+        try {
+        const response = await fetch(`http://localhost:8080/api/ip?ip=${IPAddress}`);
         const result = await response.json();
+
+        if (result.error) {
+        console.error(result.message);
+        setData(null);
+        return;
+        }
+
         setData(result);
         setIpAddress(result.ip);
+        if (result.location) {
         setLocation(`${result.location.region}, ${result.location.country} ${result.location.postalCode}`);
         setTimezone(`UTC ${result.location.timezone}`);
-        setIsp(result.isp);
         setLat(result.location.lat);
         setLon(result.location.lng);
+        } else {
+        console.error("Error: Location data is unavailable.");
+        setLocation(null);
+        setTimezone(null);
+        }
+        setIsp(result.isp);
+        } catch (error) {
+        console.error("Fetch failed:", error);
+        }
     }
 
     useEffect(() => {
@@ -50,19 +66,14 @@ export function IPGeolocationProvider({children}) {
     }, []);
 
     useEffect(() => {
-        const storedHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
-        setHistory(storedHistory);
+        fetchRecentSearches();
     },[]);
 
-    useEffect(() => {
-        if(ipAddress) {
-            setHistory((prev) => {
-                const updated = [ipAddress, ...prev.filter((i) => i!== ipAddress)].slice(0,5);
-                localStorage.setItem("searchHistory", JSON.stringify(updated));
-                return updated;
-            })
-        }
-    },[ipAddress]);
+    async function fetchRecentSearches() {
+        const res = await fetch("http://localhost:8080/api/ip/history");
+        const data = await res.json();
+        setHistory(data);
+    }
 
     return(
         <IPGeolocationContext.Provider 
