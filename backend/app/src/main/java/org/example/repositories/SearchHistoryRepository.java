87db8c0c99e8838554a8db175070b936d9ec68ed
JeffTunner <abhinavkaushik05@gmail.com>
2025-11-12 20:25:16 +0530

    Search history improvement

--- backend/app/src/main/java/org/example/repositories/SearchHistoryRepository.java
+++ backend/app/src/main/java/org/example/repositories/SearchHistoryRepository.java
@@ -4,8 +4,10 @@ import org.example.models.SearchHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface SearchHistoryRepository extends JpaRepository<SearchHistory, Long> {
 
     List<SearchHistory> findTop5ByOrderBySearchedAtDesc();
+    Optional<SearchHistory> findByIpAddress(String ipAddress);
 }
