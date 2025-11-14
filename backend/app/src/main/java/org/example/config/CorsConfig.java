fc7d52aab427c0ef0c96a7280c8bbbd9a8cab017
JeffTunner <abhinavkaushik05@gmail.com>
2025-11-14 18:54:55 +0530

    updated the cors config

--- backend/app/src/main/java/org/example/config/CorsConfig.java
+++ backend/app/src/main/java/org/example/config/CorsConfig.java
@@ -14,8 +14,8 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "http://localhost:3000")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedOrigins("https://ipadress-tracker-jtunner.netlify.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
         };
